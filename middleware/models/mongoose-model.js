a63d9f2478161681d2a67a1d0ac159cea23f57d8
PaulDepew <Paul@lostamongpines.com>
2020-07-06 10:53:06 -0700

    CRUD Functions added to server?

---
+++ middleware/models/mongoose-model.js
@@ -0,0 +1,56 @@
+'use strict';
+
+
+/**
+ * Mongo Interface class
+ * @module MongoInterface
+ */
+
+class MongoInterface {
+  constructor(schema){
+    this.schema = schema;
+  }
+
+  get(_id) {
+    let searchParam = _id ? {_id} : {};
+    return this.schema.find(searchParam)
+      .then(result => {
+        let format = {
+          count: result.length,
+          results: result,
+        };
+        return format;
+      })
+      .catch(e => console.log(e));
+  }
+
+  getByName(username) {
+    let searchParam = username ? { username } : {};
+    return this.schema.find(searchParam)
+      .then(result => {
+        let format = result[0];
+        return format;
+      })
+      .catch(e => console.log(e));
+  }
+
+  create(data) {
+    let newObject = new this.schema(data);
+    return newObject.save(newObject);
+  }
+
+  update(_id, data) {
+    return this.schema.findByIdAndUpdate(_id, data);
+  }
+
+  delete(_id) {
+    return this.schema.findByIdAndDelete(_id);
+  }
+
+  exists(data){
+    return this.schema.exists(data);
+  }
+
+}
+
+module.exports = MongoInterface;
