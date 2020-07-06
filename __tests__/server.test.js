7e580f77ff98453046119c26a38320312de63c86
PaulDepew <Paul@lostamongpines.com>
2020-07-06 15:58:55 -0700

    tests added

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,28 +1,11 @@
 'use strict';
 
 require('dotenv').config();
-const supergoose = require('cf-supergoose');
 const server = require('../lib/server');
 let request = require('supertest')(server.server);
 
 jest.spyOn(global.console, 'log');
 
-// beforeAll(() => {
-//   supergoose.startDB();
-// });
-
-// afterAll(()=> {
-//   supergoose.stopDB();
-// });
-
-// Server Start
-
-// describe('This should start our server and connect to the DB', () => {
-//   it('should respond with a console log', ()=> {
-//     server.start(3000);
-//     expect(console.log).toHaveBeenCalled();
-//   });
-// });
 
 describe('testing the server', () => {
   it('expect slash to be not found', async (done) => {
