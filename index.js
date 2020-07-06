aa9a3e96cb40d509b7b025d5432a516cdf040ef7
PaulDepew <Paul@lostamongpines.com>
2020-07-05 21:45:22 -0700

    Added README, Server start, Mongo Connection, all packages and project start info, ejs homepage template

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * Entry point into our server and connection to Mongo
+ * @module Index 
+ * @returns "Proof of Life"
+ */
+
+// NPM/MODULE REQUIREMENTS
+require('dotenv').config();
+const mongoose = require('mongoose');
+const server = require('./lib/server.js');
+
+
+// ENVIRONMENTAL VARIABLES
+const PORT = process.env.PORT || 3000;
+
+
+// MONGOOSE CONNECTION TO DB
+mongoose.connect(process.env.MONGO_ATLAS_URI, {
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+// ESTABLISHING DB AND RESPONSING WITH LIFE
+const db = mongoose.connection;
+db.on('open', () => {
+  console.log('Connected to Mongo DB');
+});
+
+// STARTING SERVER
+server.start(PORT);
