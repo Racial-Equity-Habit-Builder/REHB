cecd41c38c2af5ab24d61653a27d61a2b4cece31
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-07-06 15:55:47 -0700

    able to add a user when someone texts signup to the number

--- middleware/models/user/user-schema.js
+++ middleware/models/user/user-schema.js
@@ -8,9 +8,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true},
-  role: {type: String, required:true},
-  phoneNumber: {type: Number},
+  username: {type: String},
+  role: {type: String},
+  phoneNumber: {type: String, require: true},
   email: {type: String},
   streak: {type : Number},
   completed: {type: Array},
