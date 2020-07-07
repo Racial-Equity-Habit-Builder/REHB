ab5e28d9998ca5757b3535cb690f95f798b781ba
ashleybiermann <ashleybiermann@gmail.com>
2020-07-07 12:03:46 -0700

    successfully sending a resource via twilio to phone user

--- routes/twilio-router.js
+++ routes/twilio-router.js
@@ -9,36 +9,28 @@ const express = require('express');
 const router = express.Router();
 const { urlencoded } = require('body-parser');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-router.use(urlencoded({ extended: false}));
+router.use(urlencoded({ extended: false }));
 const UserModel = require('../middleware/models/user/user-model.js');
 const user = new UserModel();
 const ResourceModel = require('../middleware/models/resource/resource-model.js');
+const { twiml } = require('twilio');
 const resource = new ResourceModel();
-
 
 router.post('/', handleText);
 
-function handleText(req, res) {
+async function handleText(req, res) {
   const twiml = new MessagingResponse();
   let command = req.body.Body;
 
-
+  // sign up handler
   if (command.toLowerCase() === 'signup') {
-    let userDetails = {phoneNumber: req.body.From};
+    let userDetails = { phoneNumber: req.body.From };
     user.create(userDetails)
-    resource.getOne()
-    .then(result => {
-      let action = result.url;
-      console.log('this is the action:', action);
-      return action;
-    })
     twiml.message('Thanks for signing up');
-   
-  
-    // twiml.message(`Thanks for signing up. Here\'s your first action: ${action}. Respond with DONE when you\'ve completed it.`);
-  
-    
-    // console.log(req.body);
+
+    let action = await resource.getOne()
+      console.log('this is the action.url:', action.url);
+    twiml.message(action.url);
   }
 
   if (command.toLowerCase() === 'done') {
@@ -49,10 +41,8 @@ function handleText(req, res) {
     twiml.message('The options are SIGNUP, DONE, OPTIONS, and STOP.');
   }
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
   res.end(twiml.toString());
 };
-
-
 
 module.exports = router;
