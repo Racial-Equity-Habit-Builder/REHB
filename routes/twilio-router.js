d3fb16cdac5fa126e82178262d819c988bd9ce72
Dave Wolfe <dave.wolfe623@gmail.com>
2020-07-09 17:45:09 -0700

    Update twilio-router.js

--- routes/twilio-router.js
+++ routes/twilio-router.js
@@ -21,97 +21,76 @@ const { NetworkContext } = require('twilio/lib/rest/supersim/v1/network');
 router.post('/', handleText);
 
 
-async function handleText(req, res) {
+async function handleText(req, res, next) {
   try {
-  const twiml = new MessagingResponse();
-  let command = req.body.Body;
-  // let timer = cron.schedule(
-  //   '* * * * *', ()=> {
-  //     console.log('timer stuff is happening')
-  //   }, {scheduled: false}
-  // );
-  
-
-  if (command.toLowerCase() === 'signup') {
-    let userDetails = { phoneNumber: req.body.From };
-
-    let current = await user.exists(userDetails);
-    if(current === false) {
-      await user.create(userDetails);
-      twiml.message('Thanks for signing up!');
-      twiml.message('Share on twitter: ‘https://twitter.com/intent/tweet?text=Starting%20the%2021-Day%20Challenge!%20https://debbyirving.com/21-day-challenge/’');
-  
-      let action = await resource.getRandom();
-  
-      console.log('this is the action.url:', action.url);
-      twiml.message(`Get started with your first action: ${action.url}. Text back DONE when you\'ve completed it.`);
-      await user.complete(req.body.From, action._id);
-      
+    const twiml = new MessagingResponse();
+    let command = req.body.Body;
+    // let timer = cron.schedule(
+    //   '* * * * *', ()=> {
+    //     console.log('timer stuff is happening')
+    //   }, {scheduled: false}
+    // );
+    if (command.toLowerCase() === 'signup') {
       let userDetails = { phoneNumber: req.body.From };
-
       let current = await user.exists(userDetails);
-      if(current === false) {
+      if (current === false) {
         await user.create(userDetails);
-        twiml.message('Thanks for signing up');
-    
+        twiml.message('Thanks for signing up!');
+        twiml.message('Share on twitter: 'https://twitter.com/intent/tweet?text=Starting%20the%2021-Day%20Challenge!%20https://debbyirving.com/21-day-challenge/'');
         let action = await resource.getRandom();
-    
         console.log('this is the action.url:', action.url);
-        twiml.message(action.url);
+        twiml.message(`Get started with your first action: ${action.url}`);
         await user.complete(req.body.From, action._id);
-        
-        
-        
-        
-      }else {
-        console.log('already signed up');
-        twiml.message('you are already signed up');
-      }       
-
-
-  }
-  
-
-  if (command.toLowerCase() === 'done') {
-       
-    let today = new Date().getTime();
-    today = Math.floor((today/1000)/60);//turning into minutes
-    let success = await user.dateCheck(today, req.body.From);
-    if (success) {
-      await user.incrementStreak({ phoneNumber: req.body.From });
-      let streak = await user.getStreak(req.body.From);
-      if (streak === 21) {
-        twiml.message('Congratulations! You\'ve completed the 21 day habit challenge!');
-        twiml.message('Share on twitter: ‘https://twitter.com/intent/tweet?text=I%20completed%20the%2021-Day%20Challenge!%20https://debbyirving.com/21-day-challenge/’');
+        let userDetails = { phoneNumber: req.body.From };
+        let current = await user.exists(userDetails);
+        if (current === false) {
+          await user.create(userDetails);
+          twiml.message('Thanks for signing up');
+          let action = await resource.getRandom();
+          console.log('this is the action.url:', action.url);
+          twiml.message(action.url);
+          await user.complete(req.body.From, action._id);
+        }
+        } else {
+          console.log('already signed up');
+          twiml.message('you are already signed up');
+        }
+    }
+      if (command.toLowerCase() === 'done') {
+        let today = new Date().getTime();
+        today = Math.floor((today / 1000) / 60);//turning into minutes
+        let success = await user.dateCheck(today, req.body.From);
+        if (success) {
+          await user.incrementStreak({ phoneNumber: req.body.From });
+          let streak = await user.getStreak(req.body.From);
+          if (streak === 21) {
+            twiml.message('Congratulations! You\'ve completed the 21 day habit challenge!');
+            twiml.message('Share on twitter: 'https://twitter.com/intent/tweet?text=I%20completed%20the%2021-Day%20Challenge!%20https://debbyirving.com/21-day-challenge/'');
+          }
+          if (streak != 21) {
+            twiml.message(`Great job! Your current streak is ${streak} days. Keep it going.`);
+          }
+        } else {
+          twiml.message('You missed a day so your streak starts over.');
+          user.resetStreak({ phoneNumber: req.body.From });
+          user.resetCompleted({ phoneNumber: req.body.From });
+        }
       }
-      if (streak != 21){
-        twiml.message(`Great job! Your current streak is ${streak} days. Keep it going.`);
-      }
-    }else {
-      twiml.message('You missed a day so your streak starts over.');
-      user.resetStreak({ phoneNumber: req.body.From });
-      user.resetCompleted({ phoneNumber: req.body.From });
-
+        if (command.toLowerCase() === 'options') {
+          twiml.message('The options are SIGNUP, DONE, OPTIONS, and LEAVE.');
+        }
+        if (command.toLowerCase() === 'leave') {
+          let phoneNumber = req.body.From;
+          await user.deleteUser(phoneNumber);
+          twiml.message('We will miss you! Text \'signup\' anytime to start again');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/xml' });
+        res.end(twiml.toString());
+    } catch (e) {
+        console.log(e.message)
+        next(e.message)
     }
-    
-
-    if (command.toLowerCase() === 'options') {
-      twiml.message('The options are SIGNUP, DONE, OPTIONS, and LEAVE.');
-    }
-
-    if (command.toLowerCase() === 'leave') {
-      let phoneNumber = req.body.From;
-      await user.deleteUser(phoneNumber); 
-      twiml.message('We will miss you! Text \'signup\' anytime to start again');
-    }
-
-    res.writeHead(200, { 'Content-Type': 'text/xml' });
-    res.end(twiml.toString());
-  } catch (e) {
-    console.log(e.message)
-    next(e.message)
   }
-}
 
 module.exports = {
   router,
